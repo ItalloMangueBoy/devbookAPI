048c85ae77aa52b689ae1a85e9816a36d0e1e988
Emika <itallomkspop@gmail.com>
2025-01-07 00:13:04 -0300

    many token features

--- src/auth/token.go
+++ src/auth/token.go
@@ -3,14 +3,18 @@ package auth
 import (
 	"devbookAPI/src/config"
 	"devbookAPI/src/model"
+	"errors"
 	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-// Genenerates user authentication token
-func GenUserToken(user model.User) (string, error) {
+// GenToken: genenerates authentication token
+func GenToken(user model.User) (string, error) {
 	claims := jwt.MapClaims{
 		"sub": user.Id,
 		"exp": time.Now().Add(time.Hour * 48).Unix(),
@@ -20,8 +24,6 @@ func GenUserToken(user model.User) (string, error) {
 		NewWithClaims(jwt.SigningMethodHS256, claims).
 		SignedString(config.SECRET)
 
-	fmt.Printf("err: %v\n", err)
-
 	if err != nil {
 		return "", err
 	}
@@ -29,25 +31,76 @@ func GenUserToken(user model.User) (string, error) {
 	return token, nil
 }
 
-// func GetClains(tokenString string) (*jwt.Claims, error) {
-// 	doCheck := func(token *jwt.Token) (interface{}, error) {
-// 		if token.Method != jwt.SigningMethodHS256 {
-// 			return nil, fmt.Errorf("unexpected signature method: %v", token.Header["alg"])
-// 		}
+// ValidToken: Check if given token is valid
+func ValidToken(r *http.Request) error {
+	token, err := getToken(r)
+	if err != nil {
+		return err
+	}
 
-// 		return config.SECRET, nil
-// 	}
+	if _, ok := token.Claims.(jwt.MapClaims); !(ok && token.Valid) {
+		return errors.New("token format is invalid")
+	}
 
-// 	token, err := jwt.Parse(tokenString, doCheck)
-// 	if ; err != nil {
-// 		return nil, err
-// 	}
+	return nil
+}
 
-// 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-// 		fmt.Println("Token válido!")
-// 		fmt.Println("Claims:", claims)
-// 	}
+// GetAuthenticatedId: Check if given token is valid
+func GetAuthenticatedId(r *http.Request) (int64, error) {
+	token, err := getToken(r)
+	if err != nil {
+		return 0, err
+	}
 
-// 	return token, nil
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		id, err := strconv.ParseInt(fmt.Sprintf("%.0f", claims["sub"]), 10, 64)
+		if err != nil {
+			return 0, err
+		}
 
-// }
+		return id, nil
+	}
+
+	return 0, errors.New("token format is invalid")
+}
+
+// CheckUserPermision: check if current user is
+func CheckUserPermision(entityId int64, authId int64) error {
+	if authId != entityId {
+		return errors.New("authenticated user cannot use this entity")
+	}
+
+	return nil
+}
+
+func getToken(r *http.Request) (token *jwt.Token, err error) {
+	tokenString, err := getTokenString(r)
+	if err != nil {
+		return
+	}
+
+	token, err = jwt.Parse(tokenString, getSecretKey)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func getTokenString(r *http.Request) (string, error) {
+	tokenString := strings.Split(r.Header.Get("Authorization"), " ")
+
+	if len(tokenString) != 2 && tokenString[0] != "Bearer" {
+		return "", fmt.Errorf("token format is invalid")
+	}
+
+	return tokenString[1], nil
+}
+
+func getSecretKey(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("unexpected signature method: %v", token.Header["alg"])
+	}
+
+	return config.SECRET, nil
+}
